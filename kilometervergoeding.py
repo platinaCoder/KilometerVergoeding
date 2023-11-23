fea6059a066023ea1c36b4408769fff973442358
platinaCoder <nick.devries@xs4all.nl>
2023-11-23 22:03:56 +0100

    Update...

--- kilometervergoeding.py
+++ kilometervergoeding.py
@@ -6,23 +6,65 @@ chaufeur = ""
 vergoeding_per_km = 0.21
 conn = st.connection("vergoeding_db", type="sql")
 
-with conn.session as s:
-    s.execute(
-        "CREATE TABLE IF NOT EXISTS km_vergoeding (medewerker TEXT, kilometers TEXT, vergoeding TEXT);"
-    )
-    s.execute("DELETE FROM km_vergoeding;")
-    km_vergoeding = {"Chariesa": "0", "Kevin": "0", "Jurre": "0"}
-    for k in km_vergoeding:
+
+km_vergoeding = conn.query("select * from km_vergoeding")
+st.dataframe(km_vergoeding, hide_index=True)
+
+if st.button("Reset Database!"):
+    with conn.session as s:
         s.execute(
-            "INSERT INTO km_vergoeding (medewerker, kilometers, vergoeding) VALUES (:medewerker, :kilometers, :vergoeding);",
-            params=dict(
-                medewerker=k, kilometers=km_vergoeding[k], vergoeding=km_vergoeding[k]
-            ),
+            "CREATE TABLE IF NOT EXISTS km_vergoeding (medewerker TEXT, kilometers REAL, vergoeding REAL);"
+        )
+        s.execute("DELETE FROM km_vergoeding;")
+        km_vergoeding = {"Chariesa": "0", "Kevin": "0", "Jurre": "0"}
+        for k in km_vergoeding:
+            s.execute(
+                "INSERT INTO km_vergoeding (medewerker, kilometers, vergoeding) VALUES (:medewerker, :kilometers, :vergoeding);",
+                params=dict(
+                    medewerker=k,
+                    kilometers=km_vergoeding[k],
+                    vergoeding=km_vergoeding[k],
+                ),
+            )
+            s.commit()
+        st.rerun()
+
+
+def query_db(medewerker, km, kosten):
+    data_query = conn.query(
+        "SELECT * FROM km_vergoeding WHERE medewerker = :medewerker",
+        ttl=3600,
+        params=dict({"medewerker": medewerker}),
+    )
+    query_filter_km = data_query["kilometers"]
+    km_to_float = float(query_filter_km)
+    query_filter_kosten = data_query["vergoeding"]
+    kosten_to_float = float(query_filter_kosten)
+    value_sum_km = str(km + km_to_float)
+    value_sum_vergoeding = str(kosten + kosten_to_float)
+    print("kilometers: ", km_to_float)
+    print("vergoeding: ", kosten_to_float)
+    print(value_sum_km, value_sum_vergoeding)
+    print("\n")
+    st.dataframe(data_query, hide_index=True)
+    st.write(medewerker, km, kosten)
+    with conn.session as s:
+        s.execute(
+            "UPDATE km_vergoeding SET kilometers = :kilometers WHERE medewerker = :medewerker",
+            {
+                "kilometers": value_sum_km,
+                "medewerker": medewerker,
+            },
+        )
+        s.execute(
+            "UPDATE km_vergoeding SET vergoeding = :vergoeding WHERE medewerker = :medewerker",
+            {
+                "vergoeding": value_sum_vergoeding,
+                "medewerker": medewerker,
+            },
         )
         s.commit()
 
-km_vergoeding = conn.query("select * from km_vergoeding")
-st.dataframe(km_vergoeding)
 
 dag = st.selectbox(
     "Kies een dag:", ("Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag")
@@ -48,26 +90,44 @@ passagier = st.multiselect(
 if chaufeur == "Chariesa" and passagier == ["Geen"]:
     km = 70.40
     kosten = round(km * vergoeding_per_km, 2)
+    if st.button(label="Submit"):
+        query_db(chaufeur, km, kosten)
+        st.rerun()
     st.write(kosten)
 if chaufeur == "Kevin" and passagier == ["Geen"]:
     km = 70.50
     kosten = round(km * vergoeding_per_km, 2)
+    if st.button(label="Submit"):
+        query_db(chaufeur, km, kosten)
+        st.rerun()
     st.write(kosten)
 if chaufeur == "Chariesa" and passagier == ["Kevin"]:
     km = 83.60
     kosten = round(km * vergoeding_per_km, 2)
+    if st.button(label="Submit"):
+        query_db(chaufeur, km, kosten)
+        st.rerun()
     st.write(kosten)
 if chaufeur == "Kevin" and passagier == ["Chariesa"]:
     km = 79.80
     kosten = round(km * vergoeding_per_km, 2)
+    if st.button(label="Submit"):
+        query_db(chaufeur, km, kosten)
+        st.rerun()
     st.write(kosten)
 if chaufeur == "Kevin" and passagier == ["Jurre", "Chariesa"]:
     km = 82.10
     kosten = round(km * vergoeding_per_km, 2)
+    if st.button(label="Submit"):
+        query_db(chaufeur, km, kosten)
+        st.rerun()
     st.write(kosten)
 if chaufeur == "Chariesa" and passagier == ["Jurre", "Kevin"]:
     km = 85.80
     kosten = round(km * vergoeding_per_km, 2)
+    if st.button(label="Submit"):
+        query_db(chaufeur, km, kosten)
+        st.rerun()
     st.write(kosten)
 if chaufeur == "Jurre":
     st.write("Jurre rijd op kosten van de zaak!")
